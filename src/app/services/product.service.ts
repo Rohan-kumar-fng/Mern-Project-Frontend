5b101cc25da571671165d53694ae49cf0e6c50d5
Rohan-kumar <rohan.kumar.coding@gmail.com>
2024-12-15 23:25:21 +0900

    Added Routing for all category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,8 +13,8 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(Id: number): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl+"/search/findByCategoryId?id="+Id).pipe(
       map(response => response._embedded.products)
     );
   }
